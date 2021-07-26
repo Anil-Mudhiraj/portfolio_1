03a086abff555f92cbfcc7ef74f915c4d4ce7c73
Anil <anil9618454029@gmail.com>
2021-07-26 15:15:31 +0530

    create portfolio

---
+++ src/components/data/projects_data.js
@@ -0,0 +1,55 @@
+import tweetme from "../../assets/projects_images/tweetme.PNG";
+import chatapp from "../../assets/projects_images/chatapp.JPG";
+import hardware_store from "../../assets/projects_images/hardware_store.jpg";
+import portfolio from "../../assets/projects_images/portfolio.JPG";
+import new_year from "../../assets/projects_images/new_year.jpg";
+import color_classification from "../../assets/projects_images/color_classification.jpg";
+import blinking from "../../assets/projects_images/blinking.JPG";
+import COVID from "../../assets/projects_images/COVID.jpg";
+import todo from "../../assets/projects_images/todo.png";
+import crypto from "../../assets/projects_images/crypto.jpg";
+import recipe from "../../assets/projects_images/recipe.png";
+import login from "../../assets/projects_images/login.png";
+
+const data_projects = [
+  {
+    name: "COVID 19 tracker",
+    image: COVID,
+    deployed_url: "https://covid-19-tracker-by-sumit.web.app/",
+    github_url: "https://github.com/Dey-Sumit/covid-19-tracker",
+    category: ["react.js"],
+  },
+
+  {
+    name: "Todo List",
+    image: todo,
+    deployed_url: "https://dev-talks.herokuapp.com/",
+    github_url: "https://github.com/Dey-Sumit/Dev-talks",
+    category: ["node.js", "react.js"],
+  },
+
+  {
+    name: "Crypto Currency Tracker",
+    image: crypto,
+    deployed_url: "https://crypto-tracker-v1.netlify.app",
+    github_url: "",
+    category: ["node.js", "api", "react.js"],
+  },
+
+  {
+    name: "Recipe App",
+    image: recipe,
+    deployed_url: "http://sumaxtweetme.pythonanywhere.com/",
+    github_url: "https://github.com/Dey-Sumit/tweetme",
+    category: ["django", "react.js"],
+  },
+
+  {
+    name: "Login & SignIn Page",
+    image: login,
+    deployed_url: "!#",
+    category: ["html_css", "vanilla"],
+  },
+];
+
+export default data_projects;
